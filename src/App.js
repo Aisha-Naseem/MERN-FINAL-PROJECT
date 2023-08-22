ae185caffc043fb62eab6ba819d97fea5cde40d3
Ayesha Naseem <ayesha.nasim789@gmail.com>
2023-08-23 01:58:11 +0500

    to be deployed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import bootsrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Homescreen from './screens/Homescreen';
+import Cartscreen from './screens/Cartscreen';
+import Footer from './components/Footer';
+import Loginscreen from './screens/Loginscreen';
+import Registerscreen from './screens/Registerscreen';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homescreen />} />
+          <Route path="/cart" element={<Cartscreen />} />
+          <Route path="/login" element={<Loginscreen />} />
+          <Route path="/signup" element={<Registerscreen />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
